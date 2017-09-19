b72f58dc7de7f8eef333ad3fb775495e218150ee
popeil97 <alexpopeil23@gmail.com>
2017-09-19 02:41:45 -0400

    commit for assignment-4

--- server/controllers/listings.server.controller.js
+++ server/controllers/listings.server.controller.js
@@ -1,18 +1,21 @@
 
 /* Dependencies */
-var mongoose = require('mongoose'), 
+var mongoose = require('mongoose'),
     Listing = require('../models/listings.server.model.js');
+
+var config = require('../config/config.js');
 
 /*
   In this file, you should use Mongoose queries in order to retrieve/add/remove/update listings.
-  On an error you should send a 404 status code, as well as the error message. 
+  On an error you should send a 404 status code, as well as the error message.
   On success (aka no error), you should send the listing(s) as JSON in the response.
 
-  HINT: if you are struggling with implementing these functions, refer back to this tutorial 
+  HINT: if you are struggling with implementing these functions, refer back to this tutorial
   from assignment 3 https://scotch.io/tutorials/using-mongoosejs-in-node-js-and-mongodb-applications
  */
-
+//mongoose.connect(config.db.uri);
 /* Create a listing */
+mongoose.connect(config.db.uri);
 exports.create = function(req, res) {
 
   /* Instantiate a Listing */
@@ -21,7 +24,7 @@ exports.create = function(req, res) {
   /* save the coordinates (located in req.results if there is an address property) */
   if(req.results) {
     listing.coordinates = {
-      latitude: req.results.lat, 
+      latitude: req.results.lat,
       longitude: req.results.lng
     };
   }
@@ -30,9 +33,17 @@ exports.create = function(req, res) {
   listing.save(function(err) {
     if(err) {
       console.log(err);
-      res.status(400).send(err);
+      res.status(404).send(err);
     } else {
       res.json(listing);
+      // listing.save(function(err) {
+      //   if(err != null) {
+      //     console.log(err);
+      //     res.status(404).send(err);
+      //   }
+      //
+      //   console.log("Listing Saved");
+      // });
     }
   });
 };
@@ -47,6 +58,27 @@ exports.read = function(req, res) {
 exports.update = function(req, res) {
   var listing = req.listing;
 
+  listing.name = req.body.name;
+  listing.code = req.body.code;
+  listing.address = req.body.address;
+
+  if(req.results) {
+    listing.coordinates = {
+      latitude: req.results.lat,
+      longitude: req.results.lng
+    };
+  }
+
+  listing.save(function(err) {
+    if(err != null) {
+      console.log(err);
+      res.status(404).send(err);
+    }
+
+    else {
+      res.json(listing);
+    }
+  });
   /* Replace the article's properties with the new properties found in req.body */
   /* save the coordinates (located in req.results if there is an address property) */
   /* Save the article */
@@ -57,24 +89,42 @@ exports.delete = function(req, res) {
   var listing = req.listing;
 
   /* Remove the article */
+  listing.remove(function(err) {
+    if(err != null) {
+      console.log(err);
+      res.status.send(err);
+    }
+
+    else {
+      res.end();
+    }
+  });
 };
 
 /* Retreive all the directory listings, sorted alphabetically by listing code */
 exports.list = function(req, res) {
   /* Your code here */
+  Listing.find().sort('code').exec(function(err,listings) {
+    if(err != null) {
+      res.status(404).send(err);
+    }
+    else {
+      res.json(listings);
+    }
+  });
 };
 
-/* 
-  Middleware: find a listing by its ID, then pass it to the next request handler. 
+/*
+  Middleware: find a listing by its ID, then pass it to the next request handler.
 
-  HINT: Find the listing using a mongoose query, 
-        bind it to the request object as the property 'listing', 
+  HINT: Find the listing using a mongoose query,
+        bind it to the request object as the property 'listing',
         then finally call next
  */
 exports.listingByID = function(req, res, next, id) {
   Listing.findById(id).exec(function(err, listing) {
     if(err) {
-      res.status(400).send(err);
+      res.status(404).send(err);
     } else {
       req.listing = listing;
       next();
